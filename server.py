5905812e374a13c6d6a49c19d42f994e7199d0fe
hanipman <boadoct@vt.edu>
2017-10-07 22:04:36 +0000

    Pretty much finished the server

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
+# wolfram alpha: pip3 install wolframalpha
+# espeak: sudo apt-get espeak
+
 import sys
 import wolframalpha
+import os
+import subprocess
+import socket
 from serverKeys import app_id
 
 # https://gist.github.com/didler/2395703
@@ -12,26 +18,58 @@ def getopts(argv):
 	return opts
 
 myargs = getopts(sys.argv)
+if not myargs:
+	print('No arguments found')
+	sys.exit()
 if '-p' in myargs:
-	server_port = myargs['-p']
+	server_port = int(myargs['-p'])
+	myargs.pop('-p', None)
 if '-b' in myargs:
-	backlog_size = myargs['-b']
+	backlog_size = int(myargs['-b'])
+	myargs.pop('-b', None)
 if '-z' in myargs:
 	socket_size = myargs['-z']
+	myargs.pop('-z', None)
+if len(myargs.keys()) != 0:
+	print('Invalid arguments')
+	sys.exit()
 
 print(app_id)
 print(server_port)
 print(backlog_size)
 print(socket_size)
 
-# TODO
-#
-#question = ""
-#
-#client = wolframalpha.Client(app_id)
-#res = client.query(question)
-#answer = next(res.results).text
+host = '172.29.124.160'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, server_port))
+print('[Checkpoint 01] Created socket at 0.0.0.0 at port ' + str(server_port))
+s.listen(backlog_size)
+while 1:
+	print('[Checkpoint 02] Listening for client connections')
+	client, address = s.accept()
 
-#print answer
+	print('[Checkpoint 07] Accepted client connection from ' + client + ' on port ' + address)
+	question = client.recv(socket_size)
 
+	print('[Checkpoint 09] Received question: ' + question)
+	cl = wolframalpha.Client(app_id)
 
+	print('[Checkpoint 10] Sending question to Wolframalpha: ' + question)
+	res = cl.query(question)
+
+	answer = next(res.results).text
+	print('[Checkpoint 11] Received answer from Wolframalpha: ' + answer)
+
+	parsed = answer
+	print('[Checkpoint 12] Speaking answer parsed for only Alphanumeric and Space characters: ' + parsed)
+
+	answer = answer.replace(" ", "_")
+	cmd_beg = 'espeak -ven+f3 -k5 -s150 '
+	with open(os.devnull, 'w') as devnull:
+		subprocess.run(cmd_beg + answer, stdout=devnull, stderr=devnull, shell=True)
+
+	if answer:
+		print('[Checkpoint 13] Sending answer: ' + answer)
+		client.send(answer)
+
+	client.close()
