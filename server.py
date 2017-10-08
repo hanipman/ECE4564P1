6c8a5643699095e0f06400150b1a8551f5ac4c94
hanipman <boadoct@vt.edu>
2017-10-08 17:07:49 +0000

    finished and commented

--- server.py
+++ server.py
@@ -4,11 +4,14 @@
 import sys
 import wolframalpha
 import os
+import re
 import subprocess
 import socket
 from serverKeys import app_id
 
 # https://gist.github.com/didler/2395703
+# Creates a dictionary that relates the commandline option to its argument
+# does not account for empty spaces
 def getopts(argv):
 	opts ={}
 	while sys.argv:
@@ -17,6 +20,10 @@ def getopts(argv):
 		sys.argv = sys.argv[1:]
 	return opts
 
+# argument error checking
+# if the dictionary is empty, command is invalid
+# if the specified argument is found in the dictionary, it is removed from it
+# if the dictionary is not empty after removing all valid arguments, invalid arguments exist, so command is invalid
 myargs = getopts(sys.argv)
 if not myargs:
 	print('No arguments found')
@@ -34,38 +41,59 @@ if len(myargs.keys()) != 0:
 	print('Invalid arguments')
 	sys.exit()
 
+# binding sockets
 host = '172.29.124.160'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, server_port))
-print('[Checkpoint 01] Created socket at 0.0.0.0 at port ' + str(server_port))
+print('[Checkpoint 01] Created socket at ' + host + ' at port ' + str(server_port))
 s.listen(backlog_size)
+
+# start while loop for client listening
 while 1:
+	# looking for clients
 	print('[Checkpoint 02] Listening for client connections')
 	client, address = s.accept()
 
-	print('[Checkpoint 07] Accepted client connection from ' + str(client) + ' on port ' + str(address))
-	question = client.recv(int(socket_size))
+	# parsing client info
+	client_ip, client_port = str(address).split(",")
+	client_ip = client_ip[2:-1]
+	client_port = client_port[1:-1]
+	print('[Checkpoint 07] Accepted client connection from ' + client_ip + ' on port ' + client_port)
 
-	print('[Checkpoint 09] Received question: ' + str(question))
+	# obtaining question from client
+	question = client.recv(int(socket_size))
+	print('[Checkpoint 09] Received question: ' + str(question)[2:-1])
+
+	# verifying wolframalpha api
 	cl = wolframalpha.Client(app_id)
 
-	print('[Checkpoint 10] Sending question to Wolframalpha: ' + str(question))
+	# send question to wolframalpha
+	print('[Checkpoint 10] Sending question to Wolframalpha: ' + str(question)[2:-1])
 	res = cl.query(question)
 
+	# parsing answer
+	# if recieved more than one answer parses the first one
 	answer = next(res.results).text
+	if '|' in answer:
+		answer = answer.split('|', 2)
+		answer = answer[0] + answer[1].split('\n', 1)[0]
 	print('[Checkpoint 11] Received answer from Wolframalpha: ' + answer)
 
-	parsed = answer
+	# parsing answer for espeak use
+	parsed = re.sub('[^0-9a-zA-Z]+', ' ', answer)
 	print('[Checkpoint 12] Speaking answer parsed for only Alphanumeric and Space characters: ' + parsed)
-
-	answer = answer.replace(" ", "_")
 	cmd_beg = 'espeak -ven+f3 -k5 -s150 '
-	with open(os.devnull, 'w') as devnull:
-		subprocess.run(cmd_beg + answer, stdout=devnull, stderr=devnull, shell=True)
+	cmd_end = ' --stdout | aplay'
+	parsed = parsed.replace(" ", "_")
 
+	# espeak
+	with open(os.devnull, 'w') as devnull:
+		subprocess.call(cmd_beg + parsed + cmd_end, stdout=devnull, stderr=devnull, shell=True)
+
+	# send answer to client
 	if answer:
-		answer = answer.replace("_", " ")
 		print('[Checkpoint 13] Sending answer: ' + answer)
 		client.send(answer.encode())
 
+	# cut off client connection
 	client.close()
