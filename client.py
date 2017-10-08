9292e63f4c21b6539dc39b047f55e888db47c5df
Chris Boado <boadoct@vt.edu>
2017-10-08 15:36:14 -0400

    Added clientKeys and client

--- client.py
+++ client.py
@@ -1,39 +1,83 @@
-# To install tweepy: sudo pip3 tweepy
+#The following imported libraries are used for both Twitter API, ESPEAK, and Client initialization
+#To set up the twitter API I used the recommened tutorial at https://pythonprogramming.net/twitter-api-streaming-tweets-python-tutorial/
+#The first and third videos of the How to use Twitter API  v1.1 with python to stream tweets were particularly useful
+#As for initializig the client I used the example provided in class (TCP Echo Client). Where I passed in the the following informaion 
+#from the command line: Server IP, Server Port Number, Socket Size, and Hashtag.
+#
 
-import tweepy
+import subprocess
+import os
+from clientKeys import ckey,csecret,asecret,atoken #These variables are defined in a seperate file called keys.py (these are the twitter keys)
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import socket
 import sys
-from clientKeys import consumer_token, consumer_secret, access_token, access_secret
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
-#auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
-#auth.set_access_token(access_token, access_secret)
-#api = tweepy.API(auth)
 
-#search_result = api.search(search_hashtag, 2)
-#for i in search_result:
-#	print(i.text)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# https://gist.github.com/dideler/2395703
+
+# https://gist.github.com/didler/2395703 {
 def getopts(argv):
-	opts = {}
+	opts ={}
 	while sys.argv:
 		if sys.argv[0][0] == '-':
 			opts[sys.argv[0]] = sys.argv[1]
 		sys.argv = sys.argv[1:]
 	return opts
 
-
-
 myargs = getopts(sys.argv)
-if '-s' in myargs:
-	server_ip = myargs['-s']
+if not myargs:
+	print('No arguments found')
+	sys.exit()
 if '-p' in myargs:
-	server_port = myargs['-p']
-if '-z' in myargs:
-	socket_size = myargs['-z']
+	server_port = int(myargs['-p'])
+	myargs.pop('-p', None)
 if '-t' in myargs:
 	hashtag = myargs['-t']
-print(server_ip)
-print(server_port)
-print(socket_size)
-print(hashtag)
+	myargs.pop('-t', None)
+if '-z' in myargs:
+	socket_size = int(myargs['-z'])
+	myargs.pop('-z', None)
+if '-s' in myargs:
+	host = myargs['-s']
+	myargs.pop('-s', None)
+if len(myargs.keys()) != 0:
+	print('Invalid arguments')
+	sys.exit()
+# }
 
+
+class listener(StreamListener):
+	print('Listening for tweets that contain: ',hashtag)
+	def on_data(self, data):
+		user = data.split('"screen_name":"')[1].split('","')[0]
+		tweet = data.split(',"text":"')[1].split('","source')[0]
+		print('New Tweet: ',tweet,' | User: ',user)
+		tweet = tweet.replace(hashtag,"")
+		parsed = tweet.replace(" ", "_")
+		#ESPEAK
+		cmd_beg = 'espeak -ven+f3 -k5 -s150 '
+		with open(os.devnull, 'w') as devnull:
+			subprocess.run(cmd_beg + parsed, stdout=devnull, stderr=devnull, shell=True)
+		s.connect((host, server_port))
+		print('Conncting to server ',host,' on port',server_port)
+		s.send(tweet.encode())
+		print('Sending question: ',tweet)
+		data_ = s.recv(socket_size)
+		s.close()
+		data_ = str(data_)
+		data_ = data_[2:-1]
+		print('Received answer:', data_)
+	def on_error(self, status):
+		print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=[hashtag])
---
+++ clientKeys.py
@@ -0,0 +1,4 @@
+ckey="KGNlfisBGpWfAykUoUctV2ffy"
+csecret="q9esyJYzkZDErP8t3wW6PorkuMazGU7bNcO3T5bVo6U8S4wdQd"
+atoken="916136608725913600-TlqUXcNTOndFkIiYArg5W29ewpmTdsV"
+asecret="niHnzhWwOZUG6tDqYmWKyrU0DCLn6lJWFoIpIYFG4Y5BO"
